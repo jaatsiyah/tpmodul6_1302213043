c3c6398adef26b3754ac2fbcf26813601a9e1e22
jaatsiyah <jaatsiyah@student.telkomuniversity.ac.id>
2023-03-26 01:38:03 +0700

    implementasi design by contract

--- tpmodul6_1302213043/Program.cs
+++ tpmodul6_1302213043/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+using System.Diagnostics.Contracts;
+
 public class SayaTubeVideo
 {
     private int id;
@@ -7,15 +11,28 @@ public class SayaTubeVideo
 
     public SayaTubeVideo(string title)
     {
+        Contract.Requires(!string.IsNullOrEmpty(title));
+        Contract.Requires(title.Length < 100);
         Random random = new Random();
         this.title = title;
         id = random.Next(10000, 99999);
-        playCount = 0;
+        this.playCount = 0;
+        
     }
 
     public void IncreasePlayCount(int playCount)
     {
-        this.playCount = playCount;
+
+        Contract.Requires(playCount <= 10000000);
+        try
+        {
+            int check = checked(this.playCount + playCount);
+            this.playCount = check;
+        }
+        catch(OverflowException overflow)
+        {
+            Console.WriteLine("Overflow Exception : " + overflow.Message);
+        }
     }
 
     public void PrintVideoDetails()
@@ -24,6 +41,7 @@ public class SayaTubeVideo
         Console.WriteLine("ID : " + id);
         Console.WriteLine("Title : " + title);
         Console.WriteLine("Play Count : " + playCount);
+        Console.WriteLine();
     }
 }
 
@@ -32,6 +50,11 @@ public class Program
     static void Main(string[] args)
     {
         SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design By Contract - [Jaatsiyah_Maulidina_Astrianto]");
+
+        sayaTubeVideo.IncreasePlayCount(2);
         sayaTubeVideo.PrintVideoDetails();
+
+        sayaTubeVideo.IncreasePlayCount(2147483647);
+        
     }
 }
