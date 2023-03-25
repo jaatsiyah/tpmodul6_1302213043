247506dfbb81e720b4171f445171f958ecb66c9a
jaatsiyah <jaatsiyah@student.telkomuniversity.ac.id>
2023-03-25 22:19:25 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302213043/Program.cs
+++ tpmodul6_1302213043/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.title = title;
+        id = random.Next(10000, 99999);
+        playCount = 0;
+    }
+
+    public void IncreasePlayCount(int playCount)
+    {
+        this.playCount = playCount;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Details Video : ");
+        Console.WriteLine("ID : " + id);
+        Console.WriteLine("Title : " + title);
+        Console.WriteLine("Play Count : " + playCount);
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design By Contract - [Jaatsiyah_Maulidina_Astrianto]");
+        sayaTubeVideo.PrintVideoDetails();
+    }
+}
